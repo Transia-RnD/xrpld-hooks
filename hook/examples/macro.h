3581c4abf413cfee3edc36f6c5fe6b12ce69e0f8
Denis Angell <denis@harpangell.com>
2022-06-30 12:13:08 -0400

    Update macro.h

--- hook/examples/macro.h
+++ hook/examples/macro.h
@@ -247,10 +247,40 @@ int out_len = 0;\
     if (i < 0) buf[0] |= 0x80U;\
 }
 
-#define ttPAYMENT 0
-#define ttCHECK_CREATE 16
-#define ttNFT_ACCEPT_OFFER 29
+#define ttPAYMENT = 0
+#define ttESCROW_CREATE = 1
+#define ttESCROW_FINISH = 2
+#define ttACCOUNT_SET = 3
+#define ttESCROW_CANCEL = 4
+#define ttREGULAR_KEY_SET = 5
+#define ttOFFER_CREATE = 7
+#define ttOFFER_CANCEL = 8
+#define ttTICKET_CREATE = 10
+#define ttSIGNER_LIST_SET = 12
+#define ttPAYCHAN_CREATE = 13
+#define ttPAYCHAN_FUND = 14
+#define ttPAYCHAN_CLAIM = 15
+#define ttCHECK_CREATE = 16
+#define ttCHECK_CASH = 17
+#define ttCHECK_CANCEL = 18
+#define ttDEPOSIT_PREAUTH = 19
+#define ttTRUST_SET = 20
+#define ttACCOUNT_DELETE = 21
+#define ttHOOK_SET = 22
+#define ttNFTOKEN_MINT = 25
+#define ttNFTOKEN_BURN = 26
+#define ttNFTOKEN_CREATE_OFFER = 27
+#define ttNFTOKEN_CANCEL_OFFER = 28
+#define ttNFTOKEN_ACCEPT_OFFER = 29
+#define ttAMENDMENT = 100
+#define ttFEE = 101
+#define ttUNL_MODIFY = 102
+#define ttEMIT_FAILURE = 103
+    
 #define tfCANONICAL 0x80000000UL
+#define tfSELLTOKEN 0x00000001UL
+#define tfBURNABLE 0x00000001UL
+#define tfTRANSFERABLE 0x00000008UL
 
 #define atACCOUNT 1U
 #define atOWNER 2U
@@ -487,6 +517,179 @@ int out_len = 0;\
 #define _07_03_ENCODE_SIGNING_PUBKEY_NULL(buf_out )\
     ENCODE_SIGNING_PUBKEY_NULL(buf_out );
 
+// NFT TOKEN TAXON
+#define ENCODE_NFTOKEN_TAXON_SIZE 6U
+#define ENCODE_NFTOKEN_TAXON(buf_out, taxon )\
+    ENCODE_UINT32_UNCOMMON(buf_out, taxon, 0x2A );
+#define _02_42_ENCODE_NFTOKEN_TAXON(buf_out, taxon )\
+    ENCODE_NFTOKEN_TAXON(buf_out, taxon );
+
+// VL UNUNCOMMON
+#define ENCODE_VL_UNUNCOMMON(buf_out, vl, vl_len, field, field2, field3)\
+    {\
+        buf_out[0] = 0x75U;\
+        buf_out[1] = field;\
+        buf_out[2] = field2;\
+        buf_out[3] = field3;\
+        *(uint64_t*)(buf_out +  2) = *(uint64_t*)(vl +  0);\
+        *(uint64_t*)(buf_out + 10) = *(uint64_t*)(vl +  8);\
+        *(uint64_t*)(buf_out + 18) = *(uint64_t*)(vl + 16);\
+        *(uint64_t*)(buf_out + 26) = *(uint64_t*)(vl + 24);\
+        *(uint64_t*)(buf_out + 34) = *(uint64_t*)(vl + 32);\
+        *(uint64_t*)(buf_out + 42) = *(uint64_t*)(vl + 40);\
+        *(uint64_t*)(buf_out + 50) = *(uint64_t*)(vl + 48);\
+        buf_out += vl_len;\
+    }
+#define _07_XX_ENCODE_VL_UNUNCOMMON(buf_out, vl, vl_len, field, field2, field3)\
+    ENCODE_VL_UNUNCOMMON(buf_out, vl, vl_len, field, field2, field3)\
+
+// VL UNCOMMON
+#define ENCODE_VL_UNCOMMON(buf_out, vl, vl_len, field, field2)\
+    {\
+        buf_out[0] = 0x75U;\
+        buf_out[1] = field;\
+        buf_out[2] = field2;\
+        *(uint64_t*)(buf_out +  2) = *(uint64_t*)(vl +  0);\
+        *(uint64_t*)(buf_out + 10) = *(uint64_t*)(vl +  8);\
+        *(uint64_t*)(buf_out + 18) = *(uint64_t*)(vl + 16);\
+        *(uint64_t*)(buf_out + 26) = *(uint64_t*)(vl + 24);\
+        *(uint64_t*)(buf_out + 34) = *(uint64_t*)(vl + 32);\
+        *(uint64_t*)(buf_out + 42) = *(uint64_t*)(vl + 40);\
+        *(uint64_t*)(buf_out + 50) = *(uint64_t*)(vl + 48);\
+        buf_out += vl_len;\
+    }
+#define _07_XX_ENCODE_VL_UNCOMMON(buf_out, vl, vl_len, field, field2)\
+    ENCODE_VL_UNCOMMON(buf_out, vl, vl_len, field, field2)\
+
+// VL COMMON
+#define ENCODE_VL_COMMON(buf_out, vl, vl_len)\
+    {\
+        buf_out[0] = 0x75U;\
+        buf_out[1] = vl_len;\
+        *(uint64_t*)(buf_out +  2) = *(uint64_t*)(vl +  0);\
+        *(uint64_t*)(buf_out + 10) = *(uint64_t*)(vl +  8);\
+        *(uint64_t*)(buf_out + 18) = *(uint64_t*)(vl + 16);\
+        *(uint64_t*)(buf_out + 26) = *(uint64_t*)(vl + 24);\
+        *(uint64_t*)(buf_out + 34) = *(uint64_t*)(vl + 32);\
+        *(uint64_t*)(buf_out + 42) = *(uint64_t*)(vl + 40);\
+        *(uint64_t*)(buf_out + 50) = *(uint64_t*)(vl + 48);\
+        buf_out += vl_len;\
+    }
+#define _07_XX_ENCODE_VL_COMMON(buf_out, vl, vl_len)\
+    ENCODE_VL_COMMON(buf_out, vl, vl_len)\
+
+// URI
+// URI_SIZE is included in the macro.
+#define ENCODE_URI(buf_out, vl, vl_len) \
+    if (vl_len <= 193) {\
+        ENCODE_VL_COMMON(buf_out, vl, vl_len);\
+    }\
+    else if (vl_len <= 12480) {\
+        vl_len -= 193;\
+        int byte1 = (vl_len >> 8) + 193;\
+        int byte2 = vl_len & 0xFFU;\
+        ENCODE_VL_UNCOMMON(buf_out, vl, vl_len, byte1, byte2);\
+    else if (vl_len <= 918744) {\
+        vl_len -= 12481;\
+        int byte1 = 241 + (vl_len >> 16);\
+        int byte2 = (vl_len >> 8) & 0xFFU;\
+        int byte3 = vl_len & 0xFFU;\
+        ENCODE_VL_UNUNCOMMON(buf_out, vl, vl_len, byte1, byte2, byte3);\
+
+#define _07_05_ENCODE_URI(buf_out, vl, vl_len)\
+    ENCODE_URI(buf_out, vl, vl_len);
+
+    
+
+// HASH256 COMMON
+#define ENCODE_HASH256_COMMON_SIZE 33U
+#define ENCODE_HASH256_COMMON(buf_out, i, field)\
+    {\
+        uint8_t uf = field;\
+        (buf_out)[0] = 0x50U + (uf & 0x0FU);\
+        (buf_out)[1] = i[0]; \
+        (buf_out)[2] = i[1]; \
+        (buf_out)[3] = i[2]; \
+        (buf_out)[4] = i[3]; \
+        (buf_out)[5] = i[4]; \
+        (buf_out)[6] = i[5]; \
+        (buf_out)[7] = i[6]; \
+        (buf_out)[8] = i[7]; \
+        (buf_out)[9] = i[8]; \
+        (buf_out)[10] = i[9]; \
+        (buf_out)[11] = i[10]; \
+        (buf_out)[12] = i[11]; \
+        (buf_out)[13] = i[12]; \
+        (buf_out)[14] = i[13]; \
+        (buf_out)[15] = i[14]; \
+        (buf_out)[16] = i[15]; \
+        (buf_out)[17] = i[16]; \
+        (buf_out)[18] = i[17]; \
+        (buf_out)[19] = i[18]; \
+        (buf_out)[20] = i[19]; \
+        (buf_out)[21] = i[20]; \
+        (buf_out)[22] = i[21]; \
+        (buf_out)[23] = i[22]; \
+        (buf_out)[24] = i[23]; \
+        (buf_out)[25] = i[24]; \
+        (buf_out)[26] = i[25]; \
+        (buf_out)[27] = i[26]; \
+        (buf_out)[28] = i[27]; \
+        (buf_out)[29] = i[28]; \
+        (buf_out)[30] = i[29]; \
+        (buf_out)[31] = i[30]; \
+        (buf_out)[32] = i[31]; \
+        (buf_out) += ENCODE_HASH256_COMMON_SIZE;\
+    }
+#define _05_XX_ENCODE_HASH256_COMMON(buf_out, tokenid, field)\
+    ENCODE_HASH256_COMMON(buf_out, tokenid, field);
+
+// NEXT NFT ID
+// TODO: Fix buf_out[1] => The flags should be 2 bytes. I think I pass in only 1
+#define GET_NEXT_NFT_ID_SIZE 32U
+#define GET_NEXT_NFT_ID(buf_out, flags, fee, hook_accid, taxon, sequence)\
+    {\
+        (buf_out)[0] = 0x00;\
+        (buf_out)[1] = flags;\
+        (buf_out)[2] = (fee)[0];\
+        (buf_out)[3] = (fee)[1];\
+        (buf_out)[4] = (hook_accid)[0];\
+        (buf_out)[5] = (hook_accid)[1];\
+        (buf_out)[6] = (hook_accid)[2];\
+        (buf_out)[7] = (hook_accid)[3];\
+        (buf_out)[8] = (hook_accid)[4];\
+        (buf_out)[9] = (hook_accid)[5];\
+        (buf_out)[10] = (hook_accid)[6];\
+        (buf_out)[11] = (hook_accid)[7];\
+        (buf_out)[12] = (hook_accid)[8];\
+        (buf_out)[13] = (hook_accid)[9];\
+        (buf_out)[14] = (hook_accid)[10];\
+        (buf_out)[15] = (hook_accid)[11];\
+        (buf_out)[16] = (hook_accid)[12];\
+        (buf_out)[17] = (hook_accid)[13];\
+        (buf_out)[18] = (hook_accid)[14];\
+        (buf_out)[19] = (hook_accid)[15];\
+        (buf_out)[20] = (hook_accid)[16];\
+        (buf_out)[21] = (hook_accid)[17];\
+        (buf_out)[22] = (hook_accid)[18];\
+        (buf_out)[23] = (hook_accid)[19];\
+        (buf_out)[24] = ((taxon) >> 24) & 0xFFU;\
+        (buf_out)[25] = ((taxon) >> 16) & 0xFFU;\
+        (buf_out)[26] = ((taxon) >> 8) & 0xFFU;\
+        (buf_out)[27] = ((taxon) >> 0) & 0xFFU;\
+        (buf_out)[28] = ((sequence) >> 24) & 0xFFU;\
+        (buf_out)[29] = ((sequence) >> 16) & 0xFFU;\
+        (buf_out)[30] = ((sequence) >> 8) & 0xFFU;\
+        (buf_out)[31] = ((sequence) >> 0) & 0xFFU;\
+        (buf_out) += GET_NEXT_NFT_ID_SIZE;\
+    }
+
+// NFT TOKEN ID
+#define ENCODE_TOKEN_ID_SIZE 33U
+#define ENCODE_TOKEN_ID(buf_out, nftokenid)\
+    ENCODE_HASH256_COMMON(buf_out, nftokenid, 0xAU);
+#define _05_10_ENCODE_TOKEN_ID(buf_out, nftokenid)\
+    ENCODE_TOKEN_ID(buf_out, nftokenid);
 
 #ifdef HAS_CALLBACK
 #define PREPARE_PAYMENT_SIMPLE_SIZE 270U
@@ -553,5 +756,71 @@ int out_len = 0;\
     }
 
 
+// NFT MINT
+// NOTE: Transfer Fee DNE
+// NOTE: Issuer DNE
+#ifdef HAS_CALLBACK
+#define PREPARE_NFT_MINT_SIMPLE_SIZE 235U // 97 + 116 + 22
+#else
+#define PREPARE_NFT_MINT_SIMPLE_SIZE 213U // 97 + 116 + 22
+#endif
+#define PREPARE_NFT_MINT_SIMPLE(buf_out_master, flags, taxon, uri, uri_len) \
+{                                                                                            \
+    uint8_t *buf_out = buf_out_master;                                                       \
+    uint8_t acc[20];                                                                         \
+    uint32_t cls = (uint32_t)ledger_seq();                                                   \
+    hook_account(SBUF(acc));                                                                 \
+    _01_02_ENCODE_TT(buf_out, ttNFTOKEN_MINT);                                              /* uint16  | size   3 */\
+    _02_02_ENCODE_FLAGS(buf_out, flags);                                                    /* uint32  | size   5 */\
+    _02_04_ENCODE_SEQUENCE(buf_out, 0);                                                     /* uint32  | size   5 */\
+    _02_26_ENCODE_FLS(buf_out, cls + 1);                                                    /* uint32  | size   6 */\
+    _02_27_ENCODE_LLS(buf_out, cls + 5);                                                    /* uint32  | size   6 */\
+    _02_42_ENCODE_NFTOKEN_TAXON(buf_out, taxon);                                            /* amount  | size   6 */\
+    _07_05_ENCODE_URI(buf_out, uri, uri_len);                                               /* vl     |  size   ? */\
+    _06_08_ENCODE_DROPS_FEE(buf_out, 120);                                                  /* amount  | size   9 */\
+    uint8_t* fee_ptr = buf_out;\
+    _07_03_ENCODE_SIGNING_PUBKEY_NULL(buf_out);                                             /* pk      | size  35 */\
+    _08_01_ENCODE_ACCOUNT_SRC(buf_out, acc);                                                /* account | size  22 */\
+    int64_t edlen = etxn_details((uint32_t)buf_out, PREPARE_NFT_MINT_SIMPLE_SIZE);          /* emitdet | size 1?? */\
+}
+
+// NFT SELL OFFER
+// NOTE: Destination DNE
+// NOTE: Expiration DNE
+#ifdef HAS_CALLBACK
+#define PREPARE_NFT_CREATE_OFFER_SELL_SIZE 271U // 133 + 116 + 22
+#else
+#define PREPARE_NFT_CREATE_OFFER_SELL_SIZE 249U // 97 + 116 + 22
+#endif
+#define PREPARE_NFT_CREATE_OFFER_SELL(\
+    buf_out_master,\
+    flags,\
+    hook_accid,\
+    nftokenid,\
+    drops_amount_raw\
+) \
+{                                                                                            \
+    uint8_t *buf_out = buf_out_master;                                                       \
+    uint8_t acc[20];                                                                         \
+    uint32_t cls = (uint32_t)ledger_seq();                                                   \
+    hook_account(SBUF(acc));                                                                 \
+    _01_02_ENCODE_TT(buf_out, ttNFTOKEN_OFFER_CREATE);                                      /* uint16  | size   3 */\
+    _02_02_ENCODE_FLAGS(buf_out, flags);                                                    /* uint32  | size   5 */\
+    _02_04_ENCODE_SEQUENCE(buf_out, 0);                                                     /* uint32  | size   5 */\
+    _02_26_ENCODE_FLS(buf_out, cls + 1);                                                    /* uint32  | size   6 */\
+    _02_27_ENCODE_LLS(buf_out, cls + 5);                                                    /* uint32  | size   6 */\
+    _05_10_ENCODE_TOKEN_ID(buf_out, nftokenid);                                             /* amount  | size  33 */\
+    _06_01_ENCODE_DROPS_AMOUNT(buf_out, drops_amount_raw);                                  /* amount  | size   9 */\
+    uint8_t* fee_ptr = buf_out;\
+    _06_08_ENCODE_DROPS_FEE(buf_out, 0);                                                    /* amount  | size   9 */\
+    _07_03_ENCODE_SIGNING_PUBKEY_NULL(buf_out);                                             /* pk      | size  35 */\
+    _08_01_ENCODE_ACCOUNT_SRC(buf_out, acc);                                                /* account | size  22 */\
+    int64_t edlen = etxn_details((uint32_t)buf_out, PREPARE_NFT_CREATE_OFFER_SELL_SIZE);    /* emitdet | size 1?? */\
+    int64_t fee = etxn_fee_base(buf_out_master, PREPARE_NFT_CREATE_OFFER_SELL_SIZE);\ 
+    _06_08_ENCODE_DROPS_FEE(fee_ptr, fee);\
+}
+
 
 #endif
+
+
